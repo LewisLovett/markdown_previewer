3405017bae6a2e85c1d1f7cb985d56b01b1bbc97
Lewis Lovett <llovett7575@gmail.com>
2023-09-06 16:30:57 +0100

    Updated styling

--- src/container/EditorWrapper/EditorWrapper.jsx
+++ src/container/EditorWrapper/EditorWrapper.jsx
@@ -1,12 +1,15 @@
 import ToolBar from "../../components/ToolBar/ToolBar";
+import './EditorWrapper.scss';
 
-
-export default function EditorWrapper(){
+export default function EditorWrapper(textAreaContent){
 
 
     return(
-        <div>
+        <div className="editorWrapper">
             <ToolBar title={"Editor"}/>
+            <textarea className="editorWrapper__textBox" id="editor">
+            {textAreaContent}
+            </textarea>
 
         </div>
 
