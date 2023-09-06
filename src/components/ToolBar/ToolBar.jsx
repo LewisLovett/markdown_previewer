e4d5a9eeeefdb7fb709aabf24f23a7470fc4bd4d
Lewis Lovett <llovett7575@gmail.com>
2023-09-06 15:54:02 +0100

    Added structure to ToolBar

--- src/components/ToolBar/ToolBar.jsx
+++ src/components/ToolBar/ToolBar.jsx
@@ -1,12 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
   import {faFireFlameSimple,faMaximize } from '@fortawesome/free-solid-svg-icons'
+  import './ToolBar.scss'
 
 export default function ToolBar({title}){
 
     return(
-        <div className="toolbar">
-            <FontAwesomeIcon icon={faFireFlameSimple} />
-            <h1>{title}</h1>
+        <div className='toolbar'>
+            <div>
+            (<FontAwesomeIcon icon={faFireFlameSimple} />)
+            <h3 className='toolbar__title'>{title}</h3>
+            </div>
             <FontAwesomeIcon icon={faMaximize} />
         </div>
 
